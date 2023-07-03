1d98a5c552709c11d2360592515baf0c1fbd8bd3
raj74434 <rajgupta74434@gmail.com>
2023-07-03 19:25:07 +0530

    browser routing added

--- anyportfront/src/App.js
+++ anyportfront/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { Route, Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Routers>
+
+        <Route path='/login'/>
+        <Route path='/signup'/>
+        <Route path='/contact'/>
+        <Route path='/order'/>
+
+
+      </Routers>
     </div>
   );
 }
