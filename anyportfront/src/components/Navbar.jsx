59d626f8d9849fccd417fd94d870155cf83b36e2
raj74434 <rajgupta74434@gmail.com>
2023-07-04 10:36:46 +0530

    navbar added

--- anyportfront/src/components/Navbar.jsx
+++ anyportfront/src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 function Navbar() {
   return (
-    <div>
-      <h1>NavBar</h1>
+    <div className={StyleSheet.nav}>
+      <div>
+      <Link to="/contact"> <h1>Anyport</h1></Link>
+      </div>
+
+      <div>
+        <Link to="/contact"><h2>Contact our team</h2></Link>
+        <Link to="/order" > <h2>Order</h2></Link>
+        <Link to="/login"><h2>Login</h2></Link>
+        <Link to="/signup"><h2>Signup</h2></Link>
+      </div>
+      
     </div>
   )
 }
