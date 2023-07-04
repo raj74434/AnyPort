d8e9fc280f577466ab2accfaeaa1e30ea2ddf3e8
raj74434 <rajgupta74434@gmail.com>
2023-07-04 10:14:46 +0530

    routing path and element added

--- anyportfront/src/App.js
+++ anyportfront/src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-// import { Route, Router } from 'react-router-dom';
+import { Route, Router } from 'react-router-dom';
 import Order from './pages/Order';
 import React from "react"
+
 
 function App() {
   return (
     <div >
-<Order></Order>
 
+      <Router>
+        <Route path='' element={<Order></Order>}/>
+        <Route path='/login' element={<Login></Login>}/>
+        <Route path='/signup' element={<Signup></Signup>}/>
+        <Route path="/support" element={<Contact></Contact>}/>
+      </Router>
+  
     </div>
   );
 }
