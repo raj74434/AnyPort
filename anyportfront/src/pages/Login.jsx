f27add9a00867c99bf1e3659d0a873b4a113036d
raj74434 <rajgupta74434@gmail.com>
2023-07-05 17:58:28 +0530

    Login page added

--- anyportfront/src/pages/Login.jsx
+++ anyportfront/src/pages/Login.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import styles from '../stylee/login.module.css'
 
 function Login() {
   return (
     <div>
+       <Navbar></Navbar>
+       
       <div className='Loginbody'>
-        <Navbar></Navbar>
+      <h1>Happy to see you</h1>
+       <form className='loginform'>
+        <input required="true" placeholder='Enter phone number' />
+        <br/>
+        <input required="true" placeholder='Enter password'/>
+        <br/>
+        <button type='submit'>Login</button>
+       </form>
       </div>
       
     </div>
--- anyportfront/src/pages/Signup.jsx
+++ anyportfront/src/pages/Signup.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import styles from '../stylee/signup.module.css'
 function Signup() {
   return (
     <div>
-      <div className='Signupbody'>
+      <div className='signupbody'>
       <Navbar></Navbar>
+      <h1>Signup</h1>
+      <form className='signupform'>
+        <input required="true" type='text' placeholder='Enter your name'/>
+        <br/>
+        <input required="true" type='number' placeholder='Enter your number' />
+        <br/>
+        <select>
+          <option>Male</option>
+          <option>Female</option>
+        </select>
+        <br/>
+        <input required="true" type='password' placeholder='Enter your password' />
+        <br/>
+        <button type='submit' >Signup</button>
+      </form>
       </div>
     </div>
   )
