3db15faf2df42b074d2c026fdf586cd73fdbbd77
raj74434 <rajgupta74434@gmail.com>
2023-07-08 21:18:56 +0530

    login section added

--- anyportfront/src/pages/Login.jsx
+++ anyportfront/src/pages/Login.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import styles from '../stylee/login.module.css'
+import { json } from 'react-router-dom';
 
 function Login() {
+
+  const[loginKey,setLoginKey]=useState();
+
+  const [loginForm,setLoginForm]=useState({phone:"",
+   password:""});
+
+  const fillForm =(e)=>{
+    const{name,value}=e.target;
+    setLoginForm({...loginForm,[name]:value})
+  }
+
+  const doLogin=(e)=>{
+    e.preventDefault();
+
+    console.log(loginForm)
+
+    const credentials = `${loginForm.phone}:${loginForm.password}`;
+    const encodedCredentials = btoa(credentials);
+
+   const key= fetch("http://localhost:8080/signIn",{
+      method:"GET",
+      headers:{
+        "content-type":"application/json",
+        'Authorization': `Basic ${encodedCredentials}`
+
+      },
+      
+    })
+    .then((r)=>{
+      console.log(r.headers.get("sessionId"));
+      return r.json()})
+    .then((d)=>{console.log(d)})
+    .catch((m)=>{console.log(m)})
+    // const sessionId = localStorage.getItem('sessionId');
+    
+    
+    setLoginKey(key);
+
+  }
+
+  // useEffect(()=>{console.log(loginKey)},[loginKey])
+
   return (
     <div>
        <Navbar></Navbar>
@@ -10,11 +53,11 @@ function Login() {
       <div className='Loginbody'>
       <h1>Happy to see you</h1>
        <form className='loginform'>
-        <input required="true" placeholder='Enter phone number' />
+        <input name='phone' onChange={fillForm} required="true" placeholder='Enter phone number' />
         <br/>
-        <input required="true" placeholder='Enter password'/>
+        <input name='password' onChange={fillForm} required="true" placeholder='Enter password'/>
         <br/>
-        <button type='submit'>Login</button>
+        <button type='submit' onClick={doLogin}>Login</button>
        </form>
       </div>
       
