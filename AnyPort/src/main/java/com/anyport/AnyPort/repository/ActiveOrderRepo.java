0defd167809bb8a6207ce676226b6721a89fb0cf
Raj Gupta <105916680+raj74434@users.noreply.github.com>
2023-07-22 09:33:19 +0530

    Delete AnyPort directory

--- AnyPort/src/main/java/com/anyport/AnyPort/repository/ActiveOrderRepo.java
+++
@@ -1,7 +0,0 @@
-package com.anyport.AnyPort.repository;
-
-import com.anyport.AnyPort.models.ActiveOrders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActiveOrderRepo extends JpaRepository<ActiveOrders,Integer> {
-}
--- AnyPort/src/main/java/com/anyport/AnyPort/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.anyport.AnyPort.repository;
-
-import com.anyport.AnyPort.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Integer> {
-    public User findByPhone(String phone);
-
-}
