e2173c280eebc67ab99024da5f82f93b00b2079e
raj74434 <rajgupta74434@gmail.com>
2023-08-25 15:11:32 +0530

    contact section added

--- anyportfront/src/pages/Contact.jsx
+++ anyportfront/src/pages/Contact.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-// import styles from ''
+import Footer from '../components/Footer'
+import styles from '../stylee/contact.module.css'
 
 function Contact() {
   return (
     <div>
-      <div >
-        <Navbar></Navbar>
-
+      <Navbar></Navbar>
+      <div  >
+        
+        <div className={styles.contactBody} >
+          <h1>Sorry for inconvenience</h1>
+          <h3> - We will soon handle your request</h3>
+          <img src='https://ggca.us/wp-content/uploads/Sad-Emoji.png.webp'/>       
+           </div>
       </div>
-      
+      <Footer></Footer>
     </div>
   )
 }
